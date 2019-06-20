e0425cb78f8244bfe5313aa9e08a0b0a189de6d1
Alison Golden <alisong779@yahoo.com>
2019-06-20 15:57:13 +0000

    Automatically backed up by Learn

--- astro-cli/lib/astro-cli.rb
+++
@@ -1,6 +0,0 @@
-require 'pry'
-require 'open-uri'
-require 'nokogiri'
-
-require_relative './astro-cli/cli.rb'
-require_relative './astro-cli/scraper.rb'
--- astro-cli/lib/astro-cli/version.rb
+++
@@ -1,5 +0,0 @@
-module Astro
-  module Cli
-    VERSION = "0.1.0"
-  end
-end
